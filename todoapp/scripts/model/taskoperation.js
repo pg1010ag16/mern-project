1183514937daaef8665b67108f80989388fd5076
pg1010ag16 <61314093+pg1010ag16@users.noreply.github.com>
2020-07-15 22:24:52 +0530

    Update taskoperation.js

--- todoapp/scripts/model/taskoperation.js
+++ todoapp/scripts/model/taskoperation.js
@@ -5,6 +5,25 @@ const taskOperations = {
         this.tasks.push(task);
         return task;
     },
+    deleteTask(){
+        this.tasks = this.tasks.filter(taskObject=>
+          !taskObject.isMark)  ;
+  },
+    
+    toggleMarking(id){
+        let taskObject = this.tasks.find(taskObject=>taskObject.id==id);
+        taskObject.isMark = !taskObject.isMark;
+    },
+    getMark()
+    {
+        return this.tasks.
+        filter(taskObject=> taskObject.isMark==true) ;
+    },
+    getUnMark(){
+        return this.tasks.length - this.getMark() ;
+    },
+    //getCount:()=> this.tasks.length,
+    getCount:()=>taskOperations.tasks.length,
     getTasks(){
         return this.tasks;
     },
